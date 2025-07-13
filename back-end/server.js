1f3a16b03565c44ee024976acfb300700cf111cc
Cristiano Pedro <145721543+Cristiano67867@users.noreply.github.com>
2025-07-13 04:31:56 -0300

    Add files via upload
    Primeira versão do servidor 1.0

---
+++ back-end/server.js
@@ -0,0 +1,23 @@
+const WebSocket = require("ws");
+require("dotenv").config();
+
+const HOST_WEBSOCKET = process.env.HOST;
+const PORT_WEBSOCKET = process.env.PORT | 8080;
+
+const wss = new WebSocket.Server({ host: HOST_WEBSOCKET, port: PORT_WEBSOCKET });
+
+console.log("Server webesocket is run...");
+
+wss.on("connection", (ws) => {
+    console.log("Client connected!");
+
+    ws.on("message", (event) => {
+        const data = JSON.parse(event);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    });
+
+});
